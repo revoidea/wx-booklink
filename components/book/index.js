cf7003316b404c62bb47f7907e825afbf655e243
revoidea <eddiedi@126.com>
2020-11-01 12:04:25 +0800

    新增书籍详情页面

--- components/book/index.js
+++ components/book/index.js
@@ -18,6 +18,20 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    onTap(event){
+      const bid = this.properties.book.id
+      wx.navigateTo({
+        url: `/pages/book-detail/book-detail?bid=${bid}`,
+      })
 
+      /**
+       * 在组件中写跳转的缺点：
+       * 降低了组件的通用性
+       * 这种方式的优点是非常方便
+       * 服务于当前的项目   项目组件，可以在组件里写一些具体的业务
+       * 如果是通用型组件  不建议在组件里写一些具体的业务 ，可以通过监听事件的方式
+       * 
+       */
+    }
   }
 })
---
+++ components/tag/index.js
@@ -0,0 +1,30 @@
+// components/tag/index.js
+Component({
+  options:{
+    multipleSlots:true //启用slot插槽
+  },
+  /**
+   * 外部样式
+   */
+  externalClasses:['tag-class'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text:String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
