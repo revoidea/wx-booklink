ed6307497cedb4c84c29280fcab6da60fd3e0ca1
revoidea <eddiedi@126.com>
2020-11-02 08:26:41 +0800

    完善书籍详情页，新增短评功能

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,9 +1,13 @@
 import {
   BookModel
 } from '../../models/book.js'
+import {
+  LikeModel
+} from '../../models/like.js'
+
 
 const bookModel = new BookModel()
-
+let likeModel = new LikeModel()
 Page({
 
   /**
@@ -13,7 +17,8 @@ Page({
     comments:[],
     book:null,
     likeStatus:false,
-    likeCount:0
+    likeCount:0,
+    posting:false
   },
 
   /**
@@ -40,6 +45,52 @@ Page({
       this.setData({
         likeStatus:res.like_status,
         likeCount:res.fav_nums
+      })
+    })
+  },
+
+  onLike(event){
+    const like_or_cancel = event.detail.behavior
+    likeModel.like(like_or_cancel,this.data.book.id,400)
+  },
+
+  onFakePost(event){
+    this.setData({
+      posting:true
+    })
+  },
+  onCancel(event){
+    this.setData({
+      posting:false
+    })
+  },
+
+  onPost(event){
+    const comment = event.detail.text || event.detail.value
+    if(!comment){
+      return
+    }
+    if(comment.length>12){
+      wx.showToast({
+        title: '短评最多12个字',
+        icon:'none'
+      })
+      return
+    }
+    bookModel.postComment(this.data.book.id,comment)
+    .then(res => {
+      wx.showToast({
+        title: '+ 1',
+        icon:"none"
+      })
+
+      this.data.comments.unshift({
+        content:comment,
+        nums:1
+      })
+      this.setData({
+        comments:this.data.comments,
+        posting:false
       })
     })
   },
